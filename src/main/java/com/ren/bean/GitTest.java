86617d8b5340700e385e72da6eb45a2ed374deff
Ren <2717930947@qq.com>
2022-10-23 16:30:39 +0800

    IDEA Third

--- src/main/java/com/ren/bean/GitTest.java
+++ src/main/java/com/ren/bean/GitTest.java
@@ -11,5 +11,7 @@ public class GitTest {
         System.out.println("IDEA First");
         System.out.println("IDEA First");
         System.out.println("IDEA First");
+
+        System.out.println("IDEA hot-fix Fourth");
     }
 }
