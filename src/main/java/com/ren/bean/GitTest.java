46d757cd831215f093e8f8c08449ee46ebf9e1a4
Ren <2717930947@qq.com>
2022-10-23 16:12:27 +0800

    IDEA Second

---
+++ src/main/java/com/ren/bean/GitTest.java
@@ -0,0 +1,12 @@
+package com.ren.bean;
+
+/**
+ * Date:2022/10/23
+ * Author:Ren.
+ * Description:
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("IDEA First");
+    }
+}
