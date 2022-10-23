e488b6a35a8fce4e6dad7f03ccf528a4e0abe5bb
Ren <2717930947@qq.com>
2022-10-23 17:08:51 +0800

    IDEA master push github

--- src/main/java/com/ren/bean/GitTest.java
+++ src/main/java/com/ren/bean/GitTest.java
@@ -18,5 +18,7 @@ public class GitTest {
         System.out.println("hot-fix conflict test");
 
         System.out.println("master conflict test");
+
+        System.out.println("push commit test");
     }
 }
